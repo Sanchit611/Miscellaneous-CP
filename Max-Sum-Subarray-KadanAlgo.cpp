3bf8404564e9451d51ea0978fb8d191029c9f0d4
Sanchit Narang <79566726+Sanchit611@users.noreply.github.com>
2021-04-18 15:18:36 +0530

    Kadan's Algo

---
+++ Max-Sum-Subarray-KadanAlgo.cpp
@@ -0,0 +1,21 @@
+//Maximum Sum Subarray using Kadan's Algo 
+//https://leetcode.com/problems/maximum-subarray/
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+       //Kadan's Algo
+        int max_sum=INT_MIN;
+        int cur_sum=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            cur_sum+=nums[i];
+            if(cur_sum>max_sum){
+                max_sum=cur_sum;  
+            }
+            if(cur_sum<0)
+                cur_sum=0;
+        }
+        return max_sum;
+    }
+};
