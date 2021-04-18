360cf1a716b4005084a7bc2181ee87274d8d5fcd
Sanchit Narang <79566726+Sanchit611@users.noreply.github.com>
2021-04-18 15:16:09 +0530

    Sliding Window Leetcode

---
+++ Sliding-Window-Maximum(MultiSet).cpp
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/sliding-window-maximum/
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        int n=nums.size();
+        multiset<int,greater<int>> s;
+        vector<int>ans;
+        for(int i=0;i<k;i++){
+            s.insert(nums[i]);
+        }
+        ans.push_back(*s.begin());
+        for(int i=k;i<n;i++){
+            s.erase(s.lower_bound(nums[i-k]));
+            s.insert(nums[i]);
+            ans.push_back(*s.begin());
+        }
+        
+        return ans;
+    }
+};
